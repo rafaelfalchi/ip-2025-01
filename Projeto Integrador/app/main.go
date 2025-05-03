7e749167c1cf1b910006deb2eef5ec3bcd9c0a16
rafaelfalchi <rafaelfalchi@discente.ufg.br>
2025-05-03 16:18:44 -0300

    Update main.go

--- Projeto Integrador/app/main.go
+++ Projeto Integrador/app/main.go
@@ -10,4 +10,7 @@ func main() {
 	http.Handle("/", fs)
 	log.Println("Servidor rodando em http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Erro ao iniciar o servidor:", err)
+	}
 }
